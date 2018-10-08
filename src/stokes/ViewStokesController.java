e1a7003b21967e35e49484b389118ea839bef308
Hida <hidasi.gergely@gmail.com>
2018-10-08 16:30:53 +0200

    Stokes v.0.0.2.1 again person, db, tableview added, but not working

--- src/stokes/ViewStokesController.java
+++ src/stokes/ViewStokesController.java
@@ -3,25 +3,36 @@ package stokes;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 
 public class ViewStokesController implements Initializable {
     
+//<editor-fold defaultstate="collapsed" desc="FXML import">
     @FXML
     TableView itemTable;
     @FXML
-    TableView contactTable;    
+    TableView contactTable;
     @FXML
     TextField inputLastname;
     @FXML
@@ -29,7 +40,7 @@ public class ViewStokesController implements Initializable {
     @FXML
     TextField inputEmail;
     @FXML
-    TextField inputPassword;    
+    TextField inputPassword;
     @FXML
     Button addNewContactButton;
     @FXML
@@ -47,28 +58,138 @@ public class ViewStokesController implements Initializable {
     @FXML
     Button exportButton;
     @FXML
-    Button loginButton;    
+    Button loginButton;
     @FXML
     TextField emailLoginInput;
     @FXML
-    TextField pwLoginInput;  
+    TextField pwLoginInput;
     @FXML
-    Pane loginPane;   
+    Pane loginPane;
     @FXML
-    Pane registerPane;   
+    Pane registerPane;
     @FXML
-    Button registerButton;   
+    Button registerButton;
     @FXML
-    Pane itemPane;    
+    Pane itemPane;
+//</editor-fold>
 
+ 
+     DB db = new DB();
+
+    private final String MENU_CONTACTS = "Kontaktok";
+    private final String MENU_LIST = "Lista";
+    private final String MENU_EXPORT = "Exportálás";
+    private final String MENU_EXIT = "Kilépés";
+
+    private final ObservableList<Person> data = FXCollections.observableArrayList();  
+    
+    
+    public void setTableData() {
+        TableColumn lastNameCol = new TableColumn("Vezetéknév");
+        lastNameCol.setMinWidth(130);
+        lastNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+
+        lastNameCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                Person actualPerson = (Person) t.getTableView().getItems().get(t.getTablePosition().getRow());
+                actualPerson.setLastName(t.getNewValue());
+                db.updateContact(actualPerson);
+            }
+        }
+        );
+
+        TableColumn firstNameCol = new TableColumn("Keresztnév");
+        firstNameCol.setMinWidth(130);
+        firstNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+
+        firstNameCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                Person actualPerson = (Person) t.getTableView().getItems().get(t.getTablePosition().getRow());
+                actualPerson.setFirstName(t.getNewValue());
+                db.updateContact(actualPerson);
+            }
+        }
+        );
+
+        TableColumn emailCol = new TableColumn("Email cím");
+        emailCol.setMinWidth(250);
+        emailCol.setCellValueFactory(new PropertyValueFactory<Person, String>("email"));
+        emailCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        emailCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                Person actualPerson = (Person) t.getTableView().getItems().get(t.getTablePosition().getRow());
+                actualPerson.setEmail(t.getNewValue());
+                db.updateContact(actualPerson);
+            }
+        }
+        );
+
+        TableColumn removeCol = new TableColumn( "Törlés" );
+        emailCol.setMinWidth(100);
+
+        Callback<TableColumn<Person, String>, TableCell<Person, String>> cellFactory = 
+                new Callback<TableColumn<Person, String>, TableCell<Person, String>>()
+                {
+                    @Override
+                    public TableCell call( final TableColumn<Person, String> param )
+                    {
+                        final TableCell<Person, String> cell = new TableCell<Person, String>()
+                        {   
+                            final Button btn = new Button( "Törlés" );
+
+                            @Override
+                            public void updateItem( String item, boolean empty )
+                            {
+                                super.updateItem( item, empty );
+                                if ( empty )
+                                {
+                                    setGraphic( null );
+                                    setText( null );
+                                }
+                                else
+                                {
+                                    btn.setOnAction( ( ActionEvent event ) ->
+                                            {
+                                                Person person = getTableView().getItems().get( getIndex() );
+                                                data.remove(person);
+                                                db.removeContact(person);
+                                       } );
+                                    setGraphic( btn );
+                                    setText( null );
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                };
+
+        removeCol.setCellFactory( cellFactory );
+        
+        contactTable.getColumns().addAll(lastNameCol, firstNameCol, emailCol, removeCol);
+
+        data.addAll(db.getAllContacts());
+
+        contactTable.setItems(data);
+    }
     
     @FXML
     private void loginButton(ActionEvent event) {
         if (emailLoginInput.getText().equals("1") && pwLoginInput.getText().equals("1")){
             loginPane.setVisible(false);
-            mainSplit.setVisible(true);
-            itemPane.setVisible(true);            
-        System.out.println("correct password");            
+//            registerPane.setVisible(false);             
+//            itemPane.setVisible(false);   
+//            exportPane.setVisible(false);   
+            mainSplit.setVisible(true);            
+            contactPane.setVisible(true);               
         }
 
     }
@@ -80,9 +201,32 @@ public class ViewStokesController implements Initializable {
             registerPane.setVisible(true);              
     }
 
-  
+    private void alert(String text) {
+        mainSplit.setDisable(true);
+        mainSplit.setOpacity(0.4);
+        
+        Label label = new Label(text);
+        Button alertButton = new Button("OK");
+        VBox vbox = new VBox(label, alertButton);
+        vbox.setAlignment(Pos.CENTER);
+        
+        alertButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                mainSplit.setDisable(false);
+                mainSplit.setOpacity(1);
+                vbox.setVisible(false);
+            }
+        });
+        
+        anchor.getChildren().add(vbox);
+        anchor.setTopAnchor(vbox, 300.0);
+        anchor.setLeftAnchor(vbox, 300.0);
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+//    setTableData();
 
     }    
     
